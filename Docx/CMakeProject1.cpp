763c8a3af14c2851f7493f2b6eb49913a72ae79e
Kosmo <the_kosmo@outlook.com>
2023-11-21 20:03:07 +0500

    Добавьте файлы проекта.

---
+++ Docx/CMakeProject1.cpp
@@ -0,0 +1,16 @@
+﻿#include "CMakeProject1.h"
+#include <fstream>
+#include <filesystem>
+#include <duckx/duckx.hpp>
+
+int main() {
+
+    duckx::Document doc("file.docx");
+    doc.open();
+    for (auto p = doc.paragraphs(); p.has_next(); p.next()) {
+        for (auto r = p.runs(); r.has_next(); r.next()) {
+            std::cout << r.get_text() << std::endl;
+        }
+    }
+    return 0;
+}
